cb29d2ea1d60ce65fddf6db3b4ce621a9096f6db
liaojiacan <liaojiacan@qq.com>
2019-01-31 00:24:45 +0800

    leetcode: Q101对称二叉树 #4

---
+++ data-structure-and-algorithms/src/main/java/com/github/liaojiacan/lintcode/common/TreeNode.java
@@ -0,0 +1,17 @@
+package com.github.liaojiacan.lintcode.common;
+
+
+/**
+ * @author liaojiacan
+ * @date 2019/1/31
+ */
+public class TreeNode{
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
+
+	public TreeNode(int val) {
+		this.val = val;
+	}
+}
+
