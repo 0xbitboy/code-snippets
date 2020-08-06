2fbebe6243a3d7cc5f067b0861a3fb9e9982d0e3
liaojiacan <liaojiacan@qq.com>
2020-08-06 21:17:53 +0800

    fix: 解决特殊用例的问题。[6, 7, 3, 7, 2, 5, 2, 2]

--- data-structure-and-algorithms/src/main/java/com/github/liaojiacan/coding/合并两个数组然后这个数组组成的数字数值最大/Solution.java
+++ data-structure-and-algorithms/src/main/java/com/github/liaojiacan/coding/合并两个数组然后这个数组组成的数字数值最大/Solution.java
@@ -25,6 +25,24 @@ public class Solution {
         while (p1 < arr1.length && p2 < arr2.length) {
             if (arr1[p1] > arr2[p2]) {
                 ans[k++] = arr1[p1++];
+            }else if(arr1[p1] == arr2[p2]){
+               // 看下一位那个大，先选那个
+
+               if(p1+1 >= arr1.length){
+                   ans[k++] = arr2[p2++];
+                   continue;
+               }
+
+                if(p2+1 >= arr2.length){
+                    ans[k++] = arr1[p1++];
+                    continue;
+                }
+
+                if(arr1[p1+1] > arr2[p2+1]){
+                   ans[k++] = arr1[p1++];
+               }else {
+                   ans[k++] = arr2[p2++];
+               }
             } else {
                 ans[k++] = arr2[p2++];
             }
@@ -43,8 +61,8 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        int[] arr1 = {6, 2 ,2,1};
-        int[] arr2 = {6,2,2,3};
+        int[] arr1 = {6, 7, 2, 5, 2};
+        int[] arr2 = {3, 7, 2};
         System.out.println(Arrays.toString(new Solution().mergeTwoArray(arr1, arr2)));
     }
 
