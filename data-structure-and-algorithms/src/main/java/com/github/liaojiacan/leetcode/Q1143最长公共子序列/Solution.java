87bece889738bbc376fe5da995b86aa3803bef30
liaojiacan <liaojiacan@qq.com>
2020-11-02 22:19:12 +0800

    leecode： Q1143 最长公共子序列，DP table 解法

--- data-structure-and-algorithms/src/main/java/com/github/liaojiacan/leetcode/Q1143最长公共子序列/Solution.java
+++ data-structure-and-algorithms/src/main/java/com/github/liaojiacan/leetcode/Q1143最长公共子序列/Solution.java
@@ -61,9 +61,17 @@ class Solution {
         // 1. text1[i] == text2[j] 则 dp[i][j] = dp[i-1][j-1] + 1;
         // 2. text1[i] != text2[j] 则 dp[i][j] = max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]);
 
-        // 递归写法
-        int ans = dp(text1,text2, text1.length()-1, text2.length()-1);
-        return ans;
+        int dp[][] = new int[text1.length()+1][text2.length()+1];
+        for (int i = 1; i <= text1.length(); i++) {
+            for (int j = 1; j <= text2.length(); j++) {
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[text1.length()][text2.length()];
     }
 
 
@@ -77,6 +85,11 @@ class Solution {
         }
 
         return Math.max(dp(text1,text2,i,j-1),dp(text1,text2,i-1,j));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().longestCommonSubsequence("abcde","ace"));
+
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
